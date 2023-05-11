89bb84f4124610b44488e936be652577b52185d3
Yash Garg <thisisyashgarg@gmail.com>
2023-05-11 08:26:40 +0530

    resume updated

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BOOTSTRAP_FOR_SKILL_ICON = "w-12 text-4xl mx-auto inline-block ";
-export const RESUME_LINK ="https://drive.google.com/file/d/1vQ6OdN3o-Au6bWSjj4JGSdEvFGS5LdkN/view?usp=sharing";
+export const RESUME_LINK ="https://drive.google.com/file/d/1w_DEtcbSZs6ZTUe3OxO9QI7cKGejMa_2/view?usp=sharing";
 export const PROFILE_PIC ="https://res.cloudinary.com/dwwtffefs/image/upload/v1677576649/my-profile-pic/profile_ozrf3u.png";
 const data = {
   name: "Yash Garg",
